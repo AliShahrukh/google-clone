8f65518794f7b855dd2ae10cf910d6391382dcdf
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-27 13:48:56 +0530

    Homejs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,21 @@ import './Home.css';
 
 function Home() {
     return (
-        <div className="home">
+        <div className='home'>
             <h1>This is Homepage....</h1>
+            <div className='home__header'>
+                <div className='home__headerLeft'>
+                    L
+                </div>
+                <div className='home__headerRight'>
+                    R
+                </div>
+            </div>
+            <div className='home__body'>
+                b
+            </div>
         </div>
     )
 }
 
-export default Home
+export default Home;
