e5716d34d78d2f3236d5631819070209dad6c736
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-29 18:53:02 +0530

    link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div className='home'>
             <h1>This is Homepage....</h1>
             <div className='home__header'>
-            <div className='home__headerLeft'>
-                    Left
+                <div className='home__headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>Store</Link>
                 </div>
                 <div className='home__headerRight'>
-                    Right
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
                 </div>
             </div>
             <div className='home__body'>
