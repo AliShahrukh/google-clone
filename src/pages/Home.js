7ca96b456e253bd364b6f62392c4867b6fc9c596
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-29 20:16:17 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from "./Search";
 
 function Home() {
     return (
@@ -21,7 +22,10 @@ function Home() {
                 </div>
             </div>
             <div className='home__body'>
-
+                <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
+                <div className='home__inputContainer'>
+                    <Search />
+                </div>
             </div>
 
         </div>
