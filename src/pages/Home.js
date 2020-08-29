3cce22d10c1144e5a553f157d2fdc99b3a163911
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-30 01:48:07 +0530

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,6 +21,7 @@ function Home() {
                     <Avatar />
                 </div>
             </div>
+            
             <div className='home__body'>
                 <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="" />
                 <div className='home__inputContainer'>
