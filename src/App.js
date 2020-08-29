aa57d7a10655d4d23e5ff81a3b3189790a9ff847
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-08-29 18:48:50 +0530

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
