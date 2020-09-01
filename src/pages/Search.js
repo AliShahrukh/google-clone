d09d4dad3e7c599f8e9ddf4ce6aeb1d32e024893
AliShahrukh <shahrukh.ali7860@gmail.com>
2020-09-01 19:26:13 +0530

    Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,17 +4,24 @@ import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
+import { actionTypes } from '../reducer';
 
 function Search({ hideButtons = false }) {
 
+    const [{}, dispatch] = useStateValue();
     const [input, setInput] = useState("");
     const history = useHistory();
 
     const search = e => {
         e.preventDefault();
-
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+            term: input
+        });
         history.push('search');
     };
+
     return (
         <form className="search">
             <div className="search__input">
